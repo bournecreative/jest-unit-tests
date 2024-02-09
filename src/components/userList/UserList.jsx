9fb370215616cc8ad44cd7afe1c46daf5a3e331b
Christian <bournecreative@gmail.com>
2024-02-08 21:47:36 -0600

    improve how component renders feedback, add minimal css and removed unneeded

--- src/components/userList/UserList.jsx
+++ src/components/userList/UserList.jsx
@@ -1,16 +1,31 @@
+import styles from "./styles.module.css";
+
 export const UserList = ({ users }) => {
   return (
-    <table>
-      {!users.length
-        ? "No users. Please add to populate table"
-        : users.map((user) => {
-            return (
-              <tr>
-                <td>{user.userName}</td>
-                <td>{user.userEmail}</td>
-              </tr>
-            );
-          })}
-    </table>
+    <div>
+      {!users.length ? (
+        "No users. Please add to populate table"
+      ) : (
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <td>User Name</td>
+
+              <td>User Email</td>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => {
+              return (
+                <tr key={user.userName + user.userEmail}>
+                  <td>{user.userName}</td>
+                  <td>{user.userEmail}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
+    </div>
   );
 };
