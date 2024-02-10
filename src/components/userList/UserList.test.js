26f1083c4e88d9e6ce3c6bb8e990e40cd8cdab3c
Christian <bournecreative@gmail.com>
2024-02-09 19:25:11 -0600

    add additional test for userList

--- src/components/userList/UserList.test.js
+++ src/components/userList/UserList.test.js
@@ -21,3 +21,19 @@ test("render one row per user", () => {
   const rows = container.querySelectorAll("tbody tr");
   expect(rows).toHaveLength(1);
 });
+
+test("render the name and email of each user", () => {
+  const users = [
+    { userName: "luke skywalk", userEmail: "luke@jedi4Life.com" },
+    { userName: "han solo", userEmail: "solo@smuggler4Life.com" },
+  ];
+  const { container } = render(<UserList users={users} />);
+
+  for (let user of users) {
+    const name = screen.getByRole("cell", { name: user.userName });
+    const email = screen.getByRole("cell", { name: user.userEmail });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  }
+});
